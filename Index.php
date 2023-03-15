fc245ddd1bef1ce63bc7e1c021080c1ad8cd7945
DK2223 <privade14@gmail.com>
2023-03-15 11:28:44 +0300

    Added Methods and actions, Created reg and auth php files, Added submit names

--- Index.php
+++ Index.php
@@ -4,17 +4,17 @@
     <title>Login Page</title>
 </head>
 <body>
-<form style="display: inline-grid;"> <!--method="post"  action="something.php"-->
+<form style="display: inline-grid;" method="post" action="auth.php">
     Авторизация <br><br>
     Логин: <input name="login" type="text"><br>
-    Пароль: <input name="password" type="text"><br>
-<input type="submit">
+    Пароль: <input name="password" type="password"><br>
+    <input type="submit" value="вход">
 </form>
-<form style="display: inline-grid;margin-left: 100px"> <!--method="post"-->
+<form style="display: inline-grid;margin-left: 100px" method="post" action="reg.php">
     Регистрация<br><br>
     Логин: <input name="login" type="text"><br>
-    Пароль: <input name="password" type="text"><br>
-    <input type="submit">
+    Пароль: <input name="password" type="password"><br>
+    <input type="submit" value="регистрация" >
 </form>
 </body>
 </html>
