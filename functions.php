dd48b12a20ecd7047d17c375aaa2fb504f086e74
DK2223 <privade14@gmail.com>
2023-03-16 08:11:56 +0300

    Added new files, a lot of mistakes

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+require_once ('vendor/autoload.php');
+$user = $_POST['user'];
+$password = $_POST['password'];
+
+$UserKey = array_search($user,$UsersArray);
+$PasswordKey = array_search($password,$PasswordKey);
+
+
 function printr($name, bool $die = false) {
 
     echo '<pre>',print_r($name, true),'</pre>';
--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<?php
-
-    require_once dirname(__FILE__).'/functions.php';
-
-    if (count($_POST)) {
-
-        printr($_POST);
-    }
-?>
 <!doctype html>
 <html lang="ru">
 <head>
     <title>Login Page</title>
 </head>
 <body>
-<form style="display: inline-grid;">
+<form style="display: inline-grid;" method="post" action="functions.php">
     Авторизация <br><br>
     Логин: <input name="login" type="text"><br>
     Пароль: <input name="password" type="password"><br>
